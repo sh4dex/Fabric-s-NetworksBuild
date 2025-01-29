ae3a82e73198ad9e7838f24229402fb472a17617
Thomas Sorza <sthx09567541@gmail.com>
2025-01-29 12:09:37 -0500

    finished up foodcontrol contract with libraries

--- acme-network-Lab/acme-network/chaincode/foodcontrol/foodcontrol.go
+++ acme-network-Lab/acme-network/chaincode/foodcontrol/foodcontrol.go
@@ -6,12 +6,38 @@ import (
   "strconv"
 
   //from Hyperledger
-  "github.com/hyperledger/fabric-contract-api-go"
+  "github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
 //Declaring the structure for the contract to have control over the food
 type SmartContract struct{
   contractapi.Contract
+}
+
+//Food describes basic details about food
+type Food struct{
+  Farmer    string  `json:"farmer"`
+  Variety   string  `json:"cariety"`
+}
+
+func (s *SmartContract) Set {ctx contractapi.TransactionContextInterface, foodId string, farmer string, variety string } error {
+  //syntax validations
+
+  food := food {
+    Farmer : farmer,
+    Variety : variety
+  }
+
+  //foodAsBytes, _ := json.Marshal(food)
+  foodAsBytes, err := json.Marshal(food)
+  if err != nil{
+    fmt.Printf("Error at Marshal json with food: %s", err.Error())
+    return err
+  }
+
+  //Saving to the Ledger as `Key:Value`
+  return ctx.GetStub().PutState(foodId, foodAsBytes)
+
 }
 
 //Main Function 
